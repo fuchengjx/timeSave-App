3471231385b7e7f66072d7d2f79cc8229396705b
fc <327619852@qq.com>
2019-04-12 21:23:24 +0800

    stylus mine

--- src/components/User/mine.vue
+++ src/components/User/mine.vue
@@ -56,87 +56,74 @@
   }
 </script>
 
-<style scoped>
-  .wrapper{
-    max-width: 405px;
-    margin: 0 auto;
-  }
+<style lang="stylus" scoped>
+  .wrapper
+    max-width 405px
+    margin 0 auto
   a{
     text-decoration: none;
     color: #333;
   }
-  .header{
-    text-align: center;
-    font-size: 24px;
-    font-family: PingFangSC-Semibold, sans-serif;
-    border-bottom: 1px solid #f3f3f3;
-    box-shadow: 0px 1px 1px #f7f7f714;
-    padding-bottom:4px;
-    height: 1rem;
-    line-height: 1rem;
-  }
-  .personage{
-    padding-top: 11px;
-    padding-bottom: 13px;
-    border-bottom:10px solid #FAFAFA ;
-    background: #ffffff;
-    display: flex;
-    flex-direction: row;
-  }
-  .picture{
-    width: 90px;
-    height: 90px;
-    border-radius: 50%;
-    margin-left: 16px;
-    margin-right: 24px;
-  }
-  .user{
-    display: flex;
-    flex-direction: column;
-    font-family: PingFangSC-Semibold, sans-serif;
-    width:215px;
-  }
-  .userName{
-    padding-top: 10px;
-    font-size: 30px;
-  }
-  .userEmail{
-    padding-top: 10px;
-    font-size: 18px;
-  }
-  .nav{
-    border-bottom: 35px solid #FAFAFA;
-  }
-  .nav>div{
-    background: #ffffff;
-    height: 36px;
-    line-height: 36px;
-    padding-left: 16px;
-    font-size: 15px;
-  }
-  .iconfont{
-    display: inline-block;
-    float: right;
-    padding-left:17px;
-    transform: rotateY(180deg);
-  }
-  .exit{
-    text-align: center;
-    height: 50px;
-    line-height: 50px;
-    color: red;
-    font-size: 20px;
-    background: #ffffff;
-    /*border-bottom: 275px solid ;*/
-  }
-  .content{
-    max-width: 405px;
-    margin: 0 auto;
-    background:#FAFAFA;
-    position: absolute;
-    top:1.2rem;
-    bottom: 1.2rem;
-    right: 0;
-    left: 0;
-  }
+    .header
+      height 1rem
+      padding-bottom 4px
+      border-bottom 1px solid #f3f3f3
+      box-shadow 0px 1px 1px #f7f7f714
+      font-size 24px
+      font-family PingFangSC-Semibold, sans-serif
+      line-height 1rem
+      text-align center
+    .content
+      position: absolute
+      top: 1.2rem
+      bottom: 1.2rem
+      right: 0
+      left: 0
+      max-width: 405px
+      margin: 0 auto
+      background: #FAFAFA
+    .personage
+      display flex
+      flex-direction row
+      padding-top 11px
+      padding-bottom 13px
+      border-bottom 10px solid #FAFAFA
+      background #ffffff
+      .picture
+        width 90px
+        height 90px
+        border-radius 50%
+        margin-left 16px
+        margin-right 24px
+      .user
+        display flex
+        flex-direction column
+        font-family PingFangSC-Semibold, sans-serif
+        width 215px
+        .userName
+          padding-top 10px
+          font-size 30px
+        .userEmail
+          padding-top 10px
+          font-size 18px
+    .nav
+      border-bottom 35px solid #FAFAFA
+      div
+        background #ffffff
+        height 36px
+        line-height 36px
+        padding-left 16px
+        font-size 15px
+      .iconfont
+        display inline-block
+        float right
+        padding-left 17px
+        transform rotateY(180deg)
+     .exit
+       text-align center
+       height 50px
+       line-height 50px
+       color red
+       font-size 20px
+       background #ffffff
 </style>
